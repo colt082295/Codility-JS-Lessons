b5c4e294748f3c4550cfc4716980716528fc61ff
Colton Travers <contactcolt@gmail.com>
2018-03-07 20:41:59 -0600

    Add triangle solution

--- Lesson 6/triangle.js
+++ Lesson 6/triangle.js
@@ -47,4 +47,21 @@ function solution1(A) {
     return 0;
 }
 
+function solution2(A) {
+    A.sort((a, b) => (a - b))
+    
+    for (var i = 0; i < A.length - 2; i++) {
+        var p = A[i],
+            q = A[i + 1],
+            r = A[i + 2]
+
+        if (p + q > r &&
+            q + r > p &&
+            r + p > q)
+            return 1
+    }
+    
+    return 0
+}
+
 console.log(solution1([10, 2, 5, 1, 8, 20]));
